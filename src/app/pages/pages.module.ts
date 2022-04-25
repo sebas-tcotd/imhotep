c6c873d5e9179bcd5f0c50ac5d89a3e6b9e9c94b
Sebastian Vargas <vargaspizango1@gmail.com>
2022-04-25 17:13:36 -0500

    feat: add basic structure

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LandingPageComponent,
+    DashboardComponent
+  ],
   imports: [
     CommonModule
   ]
